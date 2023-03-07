63f4bd1972b2efc75e355de63a98ff88d2c816b1
Monsursaleh <salehbog130@gmail.com>
2023-03-07 23:14:09 +0200

    pokomon verson 1- layout change by  css grid

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-console.log("testing");
-fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
-  .then((response) => response.json())
-  .then((json) => {
-    datalist(json);
+const pokedex = document.getElementById("pokedex");
+
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 50; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type) => type.type.name).join(", "),
+      id: result.id,
+    }));
+    displayPokemon(pokemon);
   });
-
-let list = document.querySelector(".list");
-const datalist = (data) => {
-  console.log(data);
-  //   list.innerHTML= data.map((item,i)=>{
-  // return `<li></li>
-  // img sric`
-
-  // })
 };
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <div class='title>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+            </div>
+        </li>
+    `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
